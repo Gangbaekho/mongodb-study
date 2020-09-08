597ba2cae301e9857a693da0642542c5f1ddf245
Gangbaekho <roorooro9933@gmail.com>
2020-09-08 12:58:34 +0900

    comparison operators

--- src/playground/ReadData.js
+++ src/playground/ReadData.js
@@ -7,3 +7,24 @@ db.movies.find({name:'The Last Ship'})
 db.movies.find({runtime:60})
 
 db.movies.findOne({runtime:60})
+
+
+// 밑에 두개는 서로 같다. 그냥 밑에꺼를 써도 되겠다.
+db.movies.find({runtime:{$eq:60}})
+
+db.movies.find({runtime:60})
+
+// ne는 not equal의 줄임말이다.
+db.movies.find({runtime:{$ne:60}})
+
+//lt lower than <
+db.movies.find({runtime:{$lt:40}})
+
+//lte lower than equal <=
+db.movies.find({runtime:{$lte:42}})
+
+//gte greater than equal >=
+db.movies.find({runtime:{$gte:42}})
+
+
+
