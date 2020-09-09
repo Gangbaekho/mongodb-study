bb078b1fb193815a845ca282cd749e08c9c72602
Gangbaekho <roorooro9933@gmail.com>
2020-09-09 15:46:56 +0900

    soring with cursor

--- src/playground/Cursor.js
+++ src/playground/Cursor.js
@@ -13,3 +13,12 @@ dataCursor.forEach((doc)=>{
 
 // 다음 document가 있는지 없는지 확인 할 수 있다.
 dataCursor.hasNext()
+
+// 1은 ASC를 의미하고 -1은 DESC을 의미한다.
+// 그리고 sort()는 항상 find() 다음에 온다는 것을 기억해야 한다.
+db.movies.find().sort({"rating.average":1}).pretty()
+db.movies.find().sort({"rating.average":-1}).pretty()
+
+// 이렇게 하면 order by 를 여러개로 줄 수 있다는 것이다.
+db.movies.find().sort({"rating.average":1,runtime:-1}).pretty()
+
