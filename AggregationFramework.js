98600fdb4cef8b9b5a2f1766e6e23d77b24d4e34
Gangbaekho <roorooro9933@gmail.com>
2020-09-18 19:34:07 +0900

    working with project

--- AggregationFramework.js
+++ AggregationFramework.js
@@ -28,3 +28,19 @@ db.persons.aggregate([
     {$group:{_id:{state:"$location.state"},totalPersons:{$sum:1}}},
     {$sort:{totalPersons: -1}}
 ])
+
+// $는 이게 단순한 String이 아니라 nested document 에서
+// 사용되는 것이라고 알려주는 것이다.
+// aggregate 의 project가 일반 find method에서 쓰는 것과 다른것은
+// 아래와 같이 원래 document에 없는 field를 특정 값들의 조합으로
+// 만들 수 있다 라는 것이다. 이건 aggregate framework에서만 가능하다는 것이다.
+db.persons.aggregate([
+    {$project:{_id:0,gender:1, fullName:{$concat:["$name.first"," ","$name.last"]}}}
+])
+
+// 이런식으로 기존에 있는 값을 조작한다음에 가져올 수 도 있다는 것이다.
+// 중요한 것은 대문자로 바꾸는 것이 아니라 조작 할 수 있다는 것이다.
+db.persons.aggregate([
+    {$project:{_id:0,gender:1, fullName:{$concat:[{$toUpper:"$name.first"}," ",{$toUpper:"$name.last"}]}}}
+])
+
